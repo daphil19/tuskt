a72aa5eecead6845839cc5930280d63cf408e201
David Phillips <daphillips19@gmail.com>
2026-04-18 17:25:57 -0400

    drop deprecated kotlin targets from shared

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,7 +33,6 @@ kotlin {
     // "tiers" here are taken from https://kotlinlang.org/docs/native-target-support.html
     // and filtered by https://ktor.io/docs/client-supported-platforms.html
     // tier 1
-    macosX64()
     macosArm64()
     iosSimulatorArm64()
     iosX64()
@@ -43,11 +42,9 @@ kotlin {
     linuxX64()
     linuxArm64()
     watchosSimulatorArm64()
-    watchosX64()
     watchosArm32()
     watchosArm64()
     tvosSimulatorArm64()
-    tvosX64()
     tvosArm64()
 
     // tier 3
